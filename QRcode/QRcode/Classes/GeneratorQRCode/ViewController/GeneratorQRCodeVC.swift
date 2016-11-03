65a77103295705b7f4d561791fc690e9fe004283
小小黄人 <feixin510@qq.com>
2016-11-03 21:08:00 +0800

    框架搭建

---
+++ QRcode/QRcode/Classes/GeneratorQRCode/ViewController/GeneratorQRCodeVC.swift
@@ -0,0 +1,35 @@
+//
+//  GeneratorQRCodeVC.swift
+//  QRcode
+//
+//  Created by weirenxin on 2016/11/3.
+//  Copyright © 2016年 广西家饰宝科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class GeneratorQRCodeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
