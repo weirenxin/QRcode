f63683984a3dca319980922093a0b511fe46ab36
小小黄人 <feixin510@qq.com>
2016-11-05 14:49:41 +0800

    扫描二维码

--- QRCode_Objective-C/QRCode_Objective-C/CustomQRCodeVC.m
+++ QRCode_Objective-C/QRCode_Objective-C/CustomQRCodeVC.m
@@ -7,31 +7,47 @@
 //
 
 #import "CustomQRCodeVC.h"
+#import "XMGQRCodeTool.h"
 
 @interface CustomQRCodeVC ()
+    
+@property (weak, nonatomic) IBOutlet UIView *scanbackView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *scanViewBottomConstraint;
 
 @end
 
 @implementation CustomQRCodeVC
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    [self beginScanAnimation];
+    [self beginScan];
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+// 开始扫描
+- (void)beginScan {
+        
+    [XMGQRCodeTool sharedXMGQRCodeTool].isDrawQRCodeRect = YES;
+    [[XMGQRCodeTool sharedXMGQRCodeTool] setInsteretRect:self.scanbackView.frame];
+    [[XMGQRCodeTool sharedXMGQRCodeTool] beginScanInView:self.view result:^(NSArray<NSString *> *resultStrs) {
+        NSLog(@"%@", resultStrs);
+        [[XMGQRCodeTool sharedXMGQRCodeTool] stopScan];
+        
+    }];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    
+// 开始扫描动画
+- (void)beginScanAnimation {
+    self.scanViewBottomConstraint.constant = self.scanbackView.frame.size.height;
+    [self.view layoutIfNeeded];
+    
+    [UIView animateWithDuration:1 animations:^{
+        [UIView setAnimationRepeatCount:CGFLOAT_MAX];
+        self.scanViewBottomConstraint.constant = - self.scanbackView.frame.size.height;
+        [self.view layoutIfNeeded];
+        
+    }];
 }
-*/
+    
 
 @end
--- QRCode_Objective-C/QRCode_Objective-C/GeneratorQRCodeVC.m
+++ QRCode_Objective-C/QRCode_Objective-C/GeneratorQRCodeVC.m
@@ -7,9 +7,12 @@
 //
 
 #import "GeneratorQRCodeVC.h"
+#import <CoreImage/CoreImage.h>
 
 @interface GeneratorQRCodeVC ()
-
+    
+@property (weak, nonatomic) IBOutlet UIImageView *qrCodeView;
+    
 @end
 
 @implementation GeneratorQRCodeVC
@@ -18,20 +21,50 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self setupQRCode];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)setupQRCode {
+    
+    // 创建滤镜对象
+    CIFilter *filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
+    
+    [filter setDefaults];
+    
+    // 设置输入源
+    NSString *inputStr = @"http://www.qq.com";
+    NSData *inputData = [inputStr dataUsingEncoding:NSUTF8StringEncoding];
+    [filter setValue:inputData forKey:@"inputMessage"];
+    
+    CIImage *outputImage = [filter outputImage];
+    
+    // 显示二维码
+    self.qrCodeView.image = [self createNonInterpolatedUIImageFormCIImage:outputImage withSize:200];
 }
-*/
+    
+- (UIImage *)createNonInterpolatedUIImageFormCIImage:(CIImage *)image withSize:(CGFloat) size {
+    
+    CGRect extent = CGRectIntegral(image.extent);
+    CGFloat scale = MIN(size/CGRectGetWidth(extent), size/CGRectGetHeight(extent));
+    
+    // 1.创建bitmap;
+    size_t width = CGRectGetWidth(extent) * scale;
+    size_t height = CGRectGetHeight(extent) * scale;
+    CGColorSpaceRef cs = CGColorSpaceCreateDeviceGray();
+    CGContextRef bitmapRef = CGBitmapContextCreate(nil, width, height, 8, 0, cs, (CGBitmapInfo)kCGImageAlphaNone);
+    CIContext *context = [CIContext contextWithOptions:nil];
+    CGImageRef bitmapImage = [context createCGImage:image fromRect:extent];
+    CGContextSetInterpolationQuality(bitmapRef, kCGInterpolationNone);
+    CGContextScaleCTM(bitmapRef, scale, scale);
+    CGContextDrawImage(bitmapRef, extent, bitmapImage);
+    
+    // 2.保存bitmap到图片
+    CGImageRef scaledImage = CGBitmapContextCreateImage(bitmapRef);
+    CGContextRelease(bitmapRef);
+    CGImageRelease(bitmapImage);
+    return [UIImage imageWithCGImage:scaledImage];
+}
 
 @end
